801064b913ff668ddec3c96fc51604a35d2cc7b9
md.sharufali987@gmail.com <md.sharufali987@gmail.com>
2024-04-01 21:49:43 +0530

    solid rectangle

---
+++ p7.js
@@ -0,0 +1,12 @@
+let row=window.prompt("Enter row")
+
+let col= window.prompt("Enter col");
+
+for(let i=1; i<=row; i++){
+
+    for(let j=1 ; j<=col ; j++){
+
+         document.write("*")
+    }
+    document.write(`<br/>`)
+}
