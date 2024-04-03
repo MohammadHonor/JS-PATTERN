95397836cef38e059968b32cf3b4be777584b7aa
md.sharufali987@gmail.com <md.sharufali987@gmail.com>
2024-04-04 00:22:39 +0530

    number triangle

---
+++ p9.js
@@ -0,0 +1,9 @@
+let num= Number(window.prompt("Enter number between 1 to 9"))
+
+for( let i=1 ; i<=num ; i++){
+
+    for(let j=1 ; j<=i ; j++){
+        document.write(i)
+    }
+    document.write(`<br/>`)
+}
